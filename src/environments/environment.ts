35bd8240af661ba4a6a78b3bc4090ad07ecdf2e0
AravindhPs <psaravinth@gmail.com>
2025-05-23 08:22:15 +0530

    env change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://us-central1-pleatswithdivu.cloudfunctions.net/api/api',
+    apiUrl: 'https://us-central1-pleatswithdivu.cloudfunctions.net/api',
   };
